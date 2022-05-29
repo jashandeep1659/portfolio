df440947840e71f4243d1b6dcbdef7ec6491ed3a
Jashandeep singh <62203228+jashandeep1659@users.noreply.github.com>
2022-05-29 13:22:31 +0530

    1.1

---
+++ pages/_document.js
@@ -0,0 +1,68 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+    return (
+        <Html className="k">
+            <Head>
+                <link
+                    href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
+                    rel="stylesheet"
+                ></link>
+            </Head>
+            <body className="dark:bg-secondary_black">
+                <Main />
+                <NextScript />
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5294151828912060"
+                    crossorigin="anonymous"
+                ></script>
+            </body>
+        </Html>
+    );
+}
+
+{
+    /* <table>
+<tr>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Javascript
+    </td>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Python
+    </td>
+</tr>
+<tr>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Next JS
+    </td>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        React JS
+    </td>
+</tr>
+<tr>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Django
+    </td>   
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Django Rest Framework
+    </td>
+</tr>
+<tr>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Tailwind CSS
+    </td>
+    <td>
+        <i className="bx bxs-right-arrow text-sm mr-1"></i>
+        Bootstrap 5
+    </td>
+</tr>
+</table> */
+}
